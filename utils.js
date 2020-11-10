b9251842e41eb9398de93e2287c65bc399fe8ffd
sarah-svg <kjskillen@gmail.com>
2020-11-09 17:14:19 -0800

    location

--- utils.js
+++ utils.js
@@ -1,9 +1,9 @@
 function mungeLocation(location) {
   return {
-    formatted_query: location.display_name,
-    latitude: location.lat,
-    longitude: location.lon
-  };
+    formatted_query: location[0].display_name,
+    latitude: location[0].lat,
+    longitude: location[0].lon
+  }; 
 }
   
 module.exports = {

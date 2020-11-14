00829ebeb47a98370f019f811686ee420e90409d
sarah-svg <kjskillen@gmail.com>
2020-11-14 00:48:07 -0800

    helo

--- utils.js
+++ utils.js
@@ -29,30 +29,35 @@ function mungeYelp(yelp) {
     };
   }).slice(0, 20);
 }
-
-function mungeTrail(hiking) {
-  return hiking.data.map(item => {
-    return {       
-
+function mungeTrails(trail) {
+  return trail.trails.map(item => {
+    return {
       name: item.name,
       location: item.location,
       length: item.length,
-      stars: item.stars, 
+      stars: item.stars,
       star_votes: item.starVotes,
-      summary: item.summary, 
-      trail_url: item.trailUrl,
-      conditions: item.conditions, 
-      conditionStatus: item.conditionStatus,
-      conditionDetails: item.conditionDetails,
-      conditionDate: item.conditionDate,
-          
-
+      summary: item.summary,
+      trail_url: item.url,
+      conditions: item.conditionDetails,
+      condition_date: item.conditionDate.split(' ')[0],
+      condition_time: item.conditionDate.split(' ')[1]
     };
-  }).slice(0, 8);
+  }).slice(0, 10);
+}
+
+function mungeReviews(review) {
+  return review.businesses.map(item => {
+    return {
+      name: item.name,
+      image_url: item.image_url,
+      price: item.price,
+      rating: item.rating,
+      url: item.url
+    };
+  }).slice(0, 20);
 }
 
 
 
-
-
-module.exports = { mungeLocation, mungeWeather, mungeTrail, mungeYelp };
+module.exports = { mungeLocation, mungeWeather, mungeTrails, mungeReviews };

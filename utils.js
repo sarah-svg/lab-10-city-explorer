390ed2a43f5f6ab13a66c4bc7761c448874ca27a
sarah-svg <kjskillen@gmail.com>
2020-11-14 00:39:02 -0800

    helo

--- utils.js
+++ utils.js
@@ -16,34 +16,42 @@ function mungeWeather(weather) {
   }).slice(0, 8);
 }
 
-function mungeTrail(trail) {
-  return trail.trails.map(item => {
-    return {
-      name: item.name,
-      location: item.location,
-      length: item.length,
-      stars: item.stars,
-      star_votes: item.starVotes,
-      summary: item.summary,
-      trail_url: item.url,
-      conditions: item.conditionDetails,
-      condition_date: item.conditionDate.split(' ')[0],
-      condition_time: item.conditionDate.split(' ')[1]
-    };
-  }).slice(0, 10);
-}
-
-function mungeYelp(review) {
-  return review.businesses.map(item => {
-    return {
+function mungeYelp(yelp) {
+  return yelp.businesses.map(item => {
+    return {       
+  
       name: item.name,
       image_url: item.image_url,
-      price: item.price,
-      rating: item.rating,
-      url: item.url
+      price:  item.price,
+      rating: item.price,
+      url: item.url,
+  
     };
   }).slice(0, 20);
 }
+
+function mungeTrail(hiking) {
+  return hiking.data.map(item => {
+    return {       
+
+      name: item.name,
+      location: item.location,
+      length: item.length,
+      stars: item.stars, 
+      star_votes: item.starVotes,
+      summary: item.summary, 
+      trail_url: item.trailUrl,
+      conditions: item.conditions, 
+      conditionStatus: item.conditionStatus,
+      conditionDetails: item.conditionDetails,
+      conditionDate: item.conditionDate,
+          
+
+    };
+  }).slice(0, 8);
+}
+
+
 
 
 

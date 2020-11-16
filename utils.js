454a6844e6928a7ca5cea0fdfa7b7437c82385a7
sarah-svg <kjskillen@gmail.com>
2020-11-15 22:41:12 -0800

    hey

--- utils.js
+++ utils.js
@@ -16,48 +16,36 @@ function mungeWeather(weather) {
   }).slice(0, 8);
 }
 
-function mungeYelp(yelp) {
-  return yelp.businesses.map(item => {
-    return {       
+// function mungeYelp(yelp) {
+//   return yelp.businesses.map(item => {
+//     return {       
   
-      name: item.name,
-      image_url: item.image_url,
-      price:  item.price,
-      rating: item.price,
-      url: item.url,
+//       name: item.name,
+//       image_url: item.image_url,
+//       price:  item.price,
+//       rating: item.price,
+//       url: item.url,
   
-    };
-  }).slice(0, 20);
-}
-function mungeTrail(trail) {
-  return trail.trails.map(item => {
-    return {
-      name: item.name,
-      location: item.location,
-      length: item.length,
-      stars: item.stars,
-      star_votes: item.starVotes,
-      summary: item.summary,
-      trail_url: item.url,
-      conditions: item.conditionDetails,
-      condition_date: item.conditionDate.split(' ')[0],
-      condition_time: item.conditionDate.split(' ')[1]
-    };
-  }).slice(0, 10);
-}
-
-function mungeReviews(review) {
-  return review.businesses.map(item => {
-    return {
-      name: item.name,
-      image_url: item.image_url,
-      price: item.price,
-      rating: item.rating,
-      url: item.url
-    };
-  }).slice(0, 20);
-}
+//     };
+//   }).slice(0, 20);
+// }
+// function mungeTrail(trail) {
+//   return trail.trails.map(item => {
+//     return {
+//       name: item.name,
+//       location: item.location,
+//       length: item.length,
+//       stars: item.stars,
+//       star_votes: item.starVotes,
+//       summary: item.summary,
+//       trail_url: item.url,
+//       conditions: item.conditionDetails,
+//       condition_date: item.conditionDate.split(' ')[0],
+//       condition_time: item.conditionDate.split(' ')[1]
+//     };
+//   }).slice(0, 10);
+// }
 
 
 
-module.exports = { mungeLocation, mungeWeather, mungeTrail, mungeReviews };
+module.exports = { mungeLocation, mungeWeather };
